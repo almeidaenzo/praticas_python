e00ac083b24173dfbeadf87024068c9d696150a4
Enzo Lopes <aenzocaina@gmail.com>
2025-09-23 10:17:12 -0300

    Adicionado operacoes.py

---
+++ operacoes.py
@@ -0,0 +1,12 @@
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+
+soma = numero_1 + numero_2
+subtracao = numero_1 - numero_2
+multiplicacao = numero_1 * numero_2
+divisao = numero_1 / numero_2
+
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
